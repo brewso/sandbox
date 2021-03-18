443ffae7b1dc4e2b954728dfbca4bf425e60cbe2
brewso <ebrousseau23@gmail.com>
2021-03-17 23:32:14 -0500

    sandbox 3 solved

--- bin/sandbox3.js
+++ bin/sandbox3.js
@@ -11,6 +11,29 @@ const start = () => {
 
 const getMostCommonCharacter = (string) => {
     // Use this space to solve the problem
+    let letter = "end";
+    let array = {};
+    let count = 0;
+
+    //get each letter and its count into an object
+    for (let i = 0; i < string.length; i++) {
+        if (array[string[i]] === undefined) {
+            array[string[i]] = 1;
+        } else {
+            array[string[i]] += 1;
+        }
+    }
+
+    //get the max count
+    for (let i in array) {
+        if(array[i] > count){
+            count = array[i];
+            letter = i;
+        }
+    }
+
+
+    return letter;
 };
 
 start();
